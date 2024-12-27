ff98c9f2a699e454fbeae5dc6c5112f4e81084d2
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-12-27 16:15:25 +0700

    feat: replace header with TopMenu in DashboardLayout for improved navigation

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,11 +1,9 @@
 import { AppSidebar } from "@/components/sidebar/app-sidebar"
-import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from "@/components/ui/sidebar"
-import { DynamicBreadcrumb } from "@/components/sidebar/dynamic-breadcrumb"
+import { TopMenu } from "@/components/sidebar/top-menu"
 
 
 export default function DashboardLayout({
@@ -17,16 +15,10 @@ export default function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex justify-between px-2 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <DynamicBreadcrumb />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <TopMenu />
+        <main className="p-3">
           {children}
-        </div>
+        </main>
       </SidebarInset>
     </SidebarProvider>
   )
