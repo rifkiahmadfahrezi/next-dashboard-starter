6ea6a5cae0b25759c006fde46ca8cc78e512487d
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-12-30 04:57:03 +0700

    feat: make top menu sticky

--- src/components/sidebar/top-menu.tsx
+++ src/components/sidebar/top-menu.tsx
@@ -7,7 +7,7 @@ import { NavUser } from './nav-user'
 export const TopMenu = () => {
   return (
    <>
-      <header className="flex justify-between px-2 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <header className="flex justify-between px-2 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 top-0 sticky bg-background z-50">
          <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
