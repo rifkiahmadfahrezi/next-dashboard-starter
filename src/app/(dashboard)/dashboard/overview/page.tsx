099791699c7e2e21882767a44e2627541abb25fd
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-12-31 20:55:22 +0700

    feat: add overview page and redirect from /dashboard

---
+++ src/app/(dashboard)/dashboard/overview/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import type { Metadata } from "next"
+
+import OverviewCards from './components/overview-cards'
+import { ChartArea } from '@/components/charts/chart-area'
+import { ChartBar } from '@/components/charts/chart-bar'
+import { ChartLine } from '@/components/charts/chart-line'
+import { ChartPie } from '@/components/charts/chart-pie'
+
+export const metadata: Metadata = {
+  title: "Overview"
+}
+
+const DashboardPage = () => {
+  return (
+   <>
+      <OverviewCards />
+      <div className="my-6">
+        <h1 className='text-xl md:text-2xl font-bold'>Charts example</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
+        <ChartLine />
+        <ChartBar />
+        <ChartArea />
+        <ChartPie />
+      </div>
+   </>
+  )
+}
+
+export default DashboardPage
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,31 +1,11 @@
-import React from 'react'
-import type { Metadata } from "next"
+// import React from 'react'
+import { redirect } from 'next/navigation'
 
-import OverviewCards from './components/overview-cards'
-import { ChartArea } from '@/components/charts/chart-area'
-import { ChartBar } from '@/components/charts/chart-bar'
-import { ChartLine } from '@/components/charts/chart-line'
-import { ChartPie } from '@/components/charts/chart-pie'
-
-export const metadata: Metadata = {
-  title: "Dashboard"
-}
 
 const DashboardPage = () => {
-  return (
-   <>
-      <OverviewCards />
-      <div className="my-6">
-        <h1 className='text-xl md:text-2xl font-bold'>Charts example</h1>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
-        <ChartLine />
-        <ChartBar />
-        <ChartArea />
-        <ChartPie />
-      </div>
-   </>
-  )
+  
+  redirect('/dashboard/overview')
+  // return null
 }
 
 export default DashboardPage
