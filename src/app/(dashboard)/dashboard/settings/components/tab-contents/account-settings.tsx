2c418be8ce5caa095c2a0ff585a5f3aaa479eca3
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-01-04 07:52:51 +0700

    style: add trasition CollapsibleContent

--- src/app/(dashboard)/dashboard/settings/components/tab-contents/account-settings.tsx
+++ src/app/(dashboard)/dashboard/settings/components/tab-contents/account-settings.tsx
@@ -4,6 +4,7 @@ import { lazy, Suspense } from 'react'
 import { AccountForm } from '../forms/account-form'
 import { Button } from '@/components/ui/button'
 import { Collapsible, CollapsibleTrigger, CollapsibleContent } from '@/components/ui/collapsible'  
+import { cn } from '@/lib/utils'
 
 const PasswordForm = lazy(() => import('../forms/password-form'))
 
@@ -18,7 +19,7 @@ const AccountSettings = () => {
             Change password
          </Button>
       </CollapsibleTrigger>
-      <CollapsibleContent className='py-5 px-2'>
+      <CollapsibleContent className={cn("text-popover-foreground outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 py-3 px-2")}>
          <Suspense fallback={'Loading...'}>
             <PasswordForm />
          </Suspense>
